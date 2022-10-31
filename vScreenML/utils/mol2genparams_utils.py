bbde04976ff4ac628f5249aa51134c0e199defec
Grigorii Andrianov <27193731+gandrianov@users.noreply.github.com>
2022-10-30 22:17:12 -0400

    mol2genparams adds SMILES info into generated params

--- vScreenML/utils/mol2genparams_utils.py
+++ vScreenML/utils/mol2genparams_utils.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from rdkit import Chem
 
 def run_mol2params():
 
@@ -15,3 +16,15 @@ def run_mol2params():
 
     option = OptionClass(sys.argv)
     main(option)
+
+    # temporary solution
+
+    try:
+        mol = Chem.MolFromMol2File(option.opt.inputs[0], removeHs=False, sanitize=True)
+        smiles = Chem.MolToSmiles(mol, kekuleSmiles=True)
+    except:
+        smiles = "None"
+
+    with open(option.opt.prefix + ".params", "a") as fwr:
+        fwr.write(f"\n#SMILES: {smiles}\n")
+        fwr.close()
