a1974caf88b2aac2d9d086279d027bbb232e2cc6
Grigorii Andrianov <27193731+gandrianov@users.noreply.github.com>
2023-02-12 11:50:26 -0500

    Update rdkit_utils.py
    Quick hack to solve AL/Al problem

--- vScreenML/utils/rdkit_utils.py
+++ vScreenML/utils/rdkit_utils.py
@@ -157,7 +157,7 @@ def create_skeleton_mol(connectivity):
     chem_symbols = ["Al", 'Si', 'Se', 'Ca', 'Mg', 'Mn', 'Fe', 'Zn', 'Co', 'Cu', 'Ni', 'Cd', 'Br', 'Cl', 'C', 'H', 'O', 'N', 'P', 'S', 'I', 'F', 'B']
 
     atoms = list(set([cc for c in connectivity for cc in c[:2]]))
-    chem_symbols = [[s for s in chem_symbols if "".join([aa for aa in a if not aa.isdigit()]).startswith(s)][0] for a in atoms]
+    chem_symbols = [[s for s in chem_symbols if "".join([aa for aa in a if not aa.isdigit()]).capitalize().startswith(s)][0] for a in atoms]
 
     if len(atoms) != len(chem_symbols):
         raise Exception("Restoring connectivity failed")
